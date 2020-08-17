6f939075302b25fef8e0de3bf74306c40c5a4bf0
tnargy <clsscmstng@gmail.com>
2020-08-17 09:16:46 -0500

    Rename script

---
+++ Assets/Rename.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Rename : MonoBehaviour
+{
+    public string input;
+    public TMP_InputField inputField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Canvas>().enabled = false;
+    }
+
+    public void Save()
+    {
+        input = inputField.text;
+        inputField.text = null;
+        gameObject.GetComponent<Canvas>().enabled = false;
+    }
+}
